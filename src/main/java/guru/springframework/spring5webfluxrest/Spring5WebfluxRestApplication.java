6a1847a7cd0753704596ac23134513d90973113d
장영연 <yyjjang@wemakeprice.com>
2020-10-28 02:08:53 +0900

    aa

--- src/main/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplication.java
+++ src/main/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5WebfluxRestApplication {
-
+	/**
+	 * https://redis.io/topics/data-types-intro
+	 * https://github.com/gunayus/reactive-redis-demo/blob/ead1e1488f59ed12892a873953a176d1ef8a4d9c/src/main/java/org/springmeetup/reactiveredis/Config.java
+	 * https://redislabs.com/ebook/part-1-getting-started/chapter-1-getting-to-know-redis/1-2-what-redis-data-structures-look-like/1-2-4-hashes-in-redis/
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5WebfluxRestApplication.class, args);
 	}
--- src/test/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
+++ src/test/java/guru/springframework/spring5webfluxrest/Spring5WebfluxRestApplicationTests.java
@@ -12,7 +12,9 @@ import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import static java.time.Duration.ofSeconds;
@@ -20,180 +22,183 @@ import static java.time.Duration.ofSeconds;
 //@RunWith(SpringRunner.class)
 @SpringBootTest
 public class Spring5WebfluxRestApplicationTests {
-//
-//	@Test
-//	public void contextLoads() {
-//		int min = 1, max = 5;
-//		Flux<Integer> evenNumbers = Flux
-//				.range(min, max)
-//				.filter(x -> x % 2 == 0); // i.e. 2, 4
-//
-//		Flux<Integer> oddNumbers = Flux
-//				.range(min, max)
-//				.filter(x -> x % 2 > 0);  // ie. 1, 3, 5
-//
-//		Flux<Integer> fluxOfIntegers = evenNumbers.concatWith(oddNumbers);
-//
-//		// same StepVerifier as in "3.4. Merge"
-//		Duration very = StepVerifier.create(fluxOfIntegers)
-//				.expectNext(2)
-//				.expectNext(4)
-//				.expectNext(1)
-//				.expectNext(3)
-//				.expectNext(5)
-//				.expectComplete()
-//				.verify();
-//	}
-//
-//	@Test
-//	public void test1() {
-//		FluxSink<Integer> i;
-//
-//		Flux.range(1, 20)
-//				.doOnRequest(r -> System.out.println("Publisher / request of " + r))
-//				.doOnNext(r -> System.out.println("Publisher / element of " + r))
-//				.doOnSubscribe(subscription -> System.out.println("Publisher / subscribe is starting!"))
-//				.doOnComplete(() -> System.out.println("Publisher / complete!"))
-//				.subscribe(new BaseSubscriber<Integer>() {
-//					@Override
-//					public void hookOnSubscribe(Subscription subscription) {
-//						System.out.println("Subscriber / subscribe is starting!");
-//						request(20);
-//					}
-//
-//					@Override
-//					public void hookOnNext(Integer integer) {
-//						//int cnt = (int) (Math.random() * 10.0);
-//						//request(1);
-//						System.out.println("Subscriber / element of " + integer);
-//						//System.out.println("Cancelling after having received " + integer);
-//						//cancel();
-//					}
-//				});
-//
-//
-//	}
-//
-//	@Test
-//	public void test_index() {
-//		Flux<Tuple2<Long, Integer>> a = Flux.just(1,2)
-//				.index()
-//				.log()
-//				.map(e -> {
-//					System.out.println("index : " + e.getT1());
-//					System.out.println("val : " + e.getT2());
-//					return e;
-//				});
-//
-//		a.subscribe(i -> System.out.println(i));
-//
-//	}
-//
-//	@Test
-//	public void test_Subscriber() {
-//		Flux.just(1,2,3,4)
-//				.log()
-//				.subscribe(new Subscriber<Integer>() {
-//					private Subscription s;
-//					int onNextAmount;
-//
-//					@Override
-//					public void onSubscribe(Subscription s) {
-//						this.s = s;
-//						s.request(2);
-//					}
-//
-//					@Override
-//					public void onNext(Integer integer) {
-//						//elements.add(integer);
-//						onNextAmount++;
-//						if (onNextAmount % 2 == 0) {
-//							s.request(2);
-//						}
-//					}
-//
-//					@Override
-//					public void onError(Throwable t) {
-//
-//					}
-//
-//					@Override
-//					public void onComplete() {
-//
-//					}
-//				});
-//	}
-//
-//	@Test
-//	public void test_BaseSubscriber() {
-//		Flux.just(1,2,3,4)
-//				.log()
-//				.subscribe(new BaseSubscriber<Integer>() {
-//					int onNextAmount;
-//
-//					@Override
-//					public void hookOnSubscribe(Subscription s) {
-//						request(2);
-//					}
-//
-//					@Override
-//					public void hookOnNext(Integer integer) {
-//						onNextAmount++;
-//						if (onNextAmount % 2 == 0) {
-//							request(2);
-//						}
-//					}
-//				});
-//	}
-//
-//	@Test
-//	public void test_ConnectableFlux() {
-//		// infinite loop
-//		ConnectableFlux<Object> publish = Flux.create(fluxSink -> {
-//			while(true) {
-//				fluxSink.next(System.currentTimeMillis());
-//			}
-//		})
-//				.sample(ofSeconds(2))
-//				.publish();
-//
-//		publish.subscribe(System.out::println);
-//		publish.subscribe(System.out::println);
-//
-//		publish.connect();
-//
-//	}
-//
-//
-//	@Test
-//	public void test_share() {
-//		Flux.just(1,2,3,4)
-//				.publish()
-//				.share();
-//	}
-//
-//
-//
-//	public enum Jang {
-//		cash("a", Arrays.asList("a","b","c")),
-//		card("b", Arrays.asList("d","e"));
-//
-//		private String title;
-//		private List<String> payList;
-//
-//
-//		Jang(String a, List<String> asList) {
-//			title = a;
-//			payList = asList;
-//		}
-//
-//		public void has() {
-//			System.out.println(Jang.values());
-//		}
-//	}
-//
-//	@Test
-//	public void test_enum() {
-//
-//	}
+
+	@Test
+	public void contextLoads() {
+		int min = 1, max = 5;
+		Flux<Integer> evenNumbers = Flux
+				.range(min, max)
+				.filter(x -> x % 2 == 0); // i.e. 2, 4
+
+		Flux<Integer> oddNumbers = Flux
+				.range(min, max)
+				.filter(x -> x % 2 > 0);  // ie. 1, 3, 5
+
+		Flux<Integer> fluxOfIntegers = evenNumbers.concatWith(oddNumbers);
+
+		// same StepVerifier as in "3.4. Merge"
+		Duration very = StepVerifier.create(fluxOfIntegers)
+				.expectNext(2)
+				.expectNext(4)
+				.expectNext(1)
+				.expectNext(3)
+				.expectNext(5)
+				.expectComplete()
+				.verify();
+	}
+
+	@Test
+	public void test1() {
+		FluxSink<Integer> i;
+
+		Flux.range(1, 20)
+				.doOnRequest(r -> System.out.println("Publisher / request of " + r))
+				.doOnNext(r -> System.out.println("Publisher / element of " + r))
+				.doOnSubscribe(subscription -> System.out.println("Publisher / subscribe is starting!"))
+				.doOnComplete(() -> System.out.println("Publisher / complete!"))
+				.subscribe(new BaseSubscriber<Integer>() {
+					@Override
+					public void hookOnSubscribe(Subscription subscription) {
+						System.out.println("Subscriber / subscribe is starting!");
+						request(20);
+					}
+
+					@Override
+					public void hookOnNext(Integer integer) {
+						//int cnt = (int) (Math.random() * 10.0);
+						//request(1);
+						System.out.println("Subscriber / element of " + integer);
+						//System.out.println("Cancelling after having received " + integer);
+						//cancel();
+					}
+				});
+
+
+	}
+
+	@Test
+	public void test_index() {
+		Flux<Tuple2<Long, Integer>> a = Flux.just(1,2)
+				.index()
+				.log()
+				.map(e -> {
+					System.out.println("index : " + e.getT1());
+					System.out.println("val : " + e.getT2());
+					return e;
+				});
+
+		a.subscribe(i -> System.out.println(i));
+
+	}
+
+	@Test
+	public void test_Subscriber() {
+		Flux.just(1,2,3,4)
+				.log()
+				.subscribe(new Subscriber<Integer>() {
+					private Subscription s;
+					int onNextAmount;
+
+					@Override
+					public void onSubscribe(Subscription s) {
+						this.s = s;
+						s.request(2);
+					}
+
+					@Override
+					public void onNext(Integer integer) {
+						//elements.add(integer);
+						onNextAmount++;
+						if (onNextAmount % 2 == 0) {
+							s.request(2);
+						}
+					}
+
+					@Override
+					public void onError(Throwable t) {
+
+					}
+
+					@Override
+					public void onComplete() {
+
+					}
+				});
+	}
+
+	@Test
+	public void test_BaseSubscriber() {
+		Flux.just(1,2,3,4)
+				.log()
+				.subscribe(new BaseSubscriber<Integer>() {
+					int onNextAmount;
+
+					@Override
+					public void hookOnSubscribe(Subscription s) {
+						request(2);
+					}
+
+					@Override
+					public void hookOnNext(Integer integer) {
+						onNextAmount++;
+						if (onNextAmount % 2 == 0) {
+							request(2);
+						}
+					}
+				});
+	}
+
+	@Test
+	public void test_ConnectableFlux() {
+		// infinite loop
+		ConnectableFlux<Object> publish = Flux.create(fluxSink -> {
+			while(true) {
+				fluxSink.next(System.currentTimeMillis());
+			}
+		})
+				.sample(ofSeconds(2))
+				.publish();
+
+		publish.subscribe(System.out::println);
+		publish.subscribe(System.out::println);
+
+		publish.connect();
+
+	}
+
+
+	@Test
+	public void test_share() {
+		Flux.just(1,2,3,4)
+				.publish()
+				.share();
+	}
+
+
+
+	public enum Jang {
+		cash("a", Arrays.asList("a","b","c")),
+		card("b", Arrays.asList("d","e"));
+
+		private String title;
+		private List<String> payList;
+
+
+		Jang(String a, List<String> asList) {
+			title = a;
+			payList = asList;
+		}
+
+		public void has() {
+			System.out.println(Jang.values());
+		}
+	}
+
+	@Test
+	public void test_enum() {
+		String UP_FILENAME = "%s-REQSE-%s.%s";
+
+		String a = String.format(UP_FILENAME, "sftpMerchantId", String.format("%02d", 1), "20201026".substring(2, 8));
+		System.out.println(a);
+	}
 }
