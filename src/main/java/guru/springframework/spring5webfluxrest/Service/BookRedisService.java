eb1e0ea082cff9b21c6fd0bb2dfccb92827fe241
장영연 <yyjjang@wemakeprice.com>
2020-10-29 00:34:25 +0900

    ccc

--- src/main/java/guru/springframework/spring5webfluxrest/Service/BookRedisService.java
+++ src/main/java/guru/springframework/spring5webfluxrest/Service/BookRedisService.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webfluxrest.Service;
 
 import guru.springframework.spring5webfluxrest.dto.Book;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -12,19 +13,20 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class BookRedisService {
     private final ReactiveRedisOperations<String, Book> redisTemplate;
-
     /**
      * 레디스의 string 타입을 사용하여, 이를 삽입한다.
      */
     public Mono<Boolean> put(String key, Book val) {
-        System.out.println(String.format("key:%s val:%s", key, val));
+        log.info("key : {} / val : {}", key, val);
+
         return redisTemplate.opsForValue().set(key, val);
     }
 
     public Mono<Book> get(String key) {
-        System.out.println(String.format("key:%s", key));
+        log.info("key : {}", key);
         return redisTemplate.opsForValue().get(key);
     }
 
@@ -38,7 +40,7 @@ public class BookRedisService {
         Map<String, String> result = new HashMap<>();
         result.put("a","b");
         result.put("c","d");
-        System.out.println(String.format("key:%s val:%s", key, val));
+        log.info("key : {} / val : {}", key, val);
         return redisTemplate.opsForHash().putAll(key, result);
     }
 
@@ -46,12 +48,12 @@ public class BookRedisService {
      * 리턴값이 false 인 것이 특이하지만, 값 수정은 제대로 됨
      */
     public Mono<Boolean> modifyHashData(String key, String hashKey, String hashVal) {
-        System.out.println(String.format("key:%s hashKey:%s hashVal:%s", key, hashKey, hashVal));
+        log.info("key : {} / hashKey : {} / hashVal : {}", key, hashKey, hashVal);
         return redisTemplate.opsForHash().put(key, hashKey, hashVal);
     }
 
     public Mono<Object> getHashData(String key, String hashKey) {
-        System.out.println(String.format("key:%s hashKey:%s", key, hashKey));
+        log.info("key : {} / hashKey : {}", key, hashKey);
         return redisTemplate.opsForHash().get(key, hashKey);
     }
 
